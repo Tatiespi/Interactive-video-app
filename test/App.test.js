bbc7e734f6ba56d049cd66a655ec71a86034807f
Daniel Andrés Vélez Vargas <daniel_andres.velez@uao.edu.co>
2024-03-04 01:58:22 -0500

    [ADD] Testing

---
+++ test/App.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import App from "../App";
+
+describe("<App />", () => {
+  it("renders Drag&Drop, Questions and their feedback", () => {
+    const tree = renderer.create(<App />).toJSON();
+
+    // Verifica que el árbol tenga contenido
+    expect(tree).toBeTruthy();
+
+    // Verifica que el árbol tenga exactamente un hijo
+    expect(tree.children.length).toBe(1);
+
+    // Verifica que el hijo de App tenga el tipo y props esperados
+    const childComponent = tree.children[0];
+    expect(childComponent.type).toBe("View");
+    // Añadir más expectativas para validar las props del hijo si es necesario
+
+    // Añadir expectativas para validar otras propiedades o comportamientos del árbol de componentes si es necesario
+  });
+});
